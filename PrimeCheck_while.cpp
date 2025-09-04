19831bc7e40d9ef93387b02a0708399d198b0a1a
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-04 12:50:07 +0600

    solve 1st problem of while loop in C++

--- PrimeCheck_while.cpp
+++ PrimeCheck_while.cpp
@@ -8,7 +8,24 @@
 #include <iostream>
 using namespace std;
 int main(){
+    int  n;
+    cout<<"Enter Number " ;
+    cin>> n;
+    if(n<=1){
+        cout<<"Not Prime Number " ;
+    }
+    int i = 2;
+    while(i<n){
+        if(n%i == 0){
+            cout << n << " is NOT a Prime Number" << endl;
+            return 0;
+            
+        }
+        i++;
+        
+    }
+
+    cout << n << " is  a Prime Number" << endl;
     return 0 ;
 }
 
-solve 1st problem of while loop in C++
