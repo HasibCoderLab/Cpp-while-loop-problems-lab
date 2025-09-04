ad27310feba9d9e28c76b5120f6c1666d7dcf8fc
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-04 11:36:21 +0600

    solve 1st problem of while loop in C++

---
+++ Print1toN_while.cpp
@@ -0,0 +1,21 @@
+// ================================================
+//               Language: C++ / Cpp
+//               Topic   : [While loop]
+//               Problem : 👉👉 [1]
+//               Want    : Print1toN_while.cpp
+//                        
+// ================================================
+#include <iostream>
+using namespace std;
+int main(){
+    int n ;
+    cout<<"Enter  Number " ;
+     cin>> n;
+     int i = 1 ;
+     while(i<=n){
+        cout << i << " " << endl;
+        i++;
+
+     }
+    return 0 ;
+}
---
+++ extra.txt
@@ -0,0 +1,12 @@
+// ================================================
+//               Language: C++ / Cpp
+//               Topic   : [ForLoop]
+//               Problem : 👉👉 [1]
+//               Want    : 
+//                        
+// ================================================
+#include <iostream>
+using namespace std;
+int main(){
+    return 0 ;
+}
