47680284c7faf67b409e48d1456fd7950941532a
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-04 13:08:52 +0600

    solve 1st problem of while loop in C++

--- DigitSum_while.cpp
+++ DigitSum_while.cpp
@@ -8,7 +8,19 @@
 #include <iostream>
 using namespace std;
 int main(){
+    int  x ;
+    cout<<"Enter  Number : " ;
+    cin>> x;
+    int i = 1;
+   int  sum = 0;
+    while(x>0){
+        int digit = x%10;
+        sum += digit;
+       x =  x /10 ;
+        
+        i++;
+    }
+    cout << "Digit Sum = " << sum << endl;
+
     return 0 ;
 }
-
-solve 1st problem of while loop in C++
