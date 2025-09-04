350c44d78a57dab0c7797d4a929ef30524e6e4f1
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-04 11:47:20 +0600

    solve 1st problem of while loop in C++

--- SumOfN_while.cpp
+++ SumOfN_while.cpp
@@ -1,6 +1,6 @@
 // ================================================
 //               Language: C++ / Cpp
-//               Topic   : [ForLoop]
+//               Topic   : [while loop]
 //               Problem : 👉👉 [2]
 //               Want    : SumOfN_while.cpp
 //                        
@@ -8,7 +8,16 @@
 #include <iostream>
 using namespace std;
 int main(){
+    int n;
+    cout<<"Enter number : ";
+    cin>> n;
+    int sum  = 0; 
+    int i = 1;
+    while(i<=n){
+        sum+=i;
+       
+        i++;
+    }
+    cout<<"Sum of 1  to  " << n  <<" is " << sum <<endl; 
     return 0 ;
 }
-
-solve 1st problem of while loop in C++"
